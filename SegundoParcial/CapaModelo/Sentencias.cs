9d62c2f8d672ec49236c9511ecf26798f6cbf46f
Joshua53Barrios <jbarrioso3@miumg.edu.gt>
2022-09-27 10:40:40 -0600

    Subiendo proyecto

---
+++ SegundoParcial/CapaModelo/Sentencias.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+namespace CapaModelo
+{
+    class Sentencias
+    {
+        Conexion con = new Conexion();
+        public OdbcDataAdapter llenartabla(string tabla)
+        {
+            string sql = "select * from " + tabla + ";";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, con.conexion());
+            return datatable;
+        }
+    }
+}
